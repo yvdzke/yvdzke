1442d1aa20dee5183339c51fa00f6255300b3a7e
yvdzke <128133701+yvdzke@users.noreply.github.com>
2023-03-29 18:56:38 +0700

    Delete scriptnav.js

--- assets/scriptnav.js
+++
@@ -1,9 +0,0 @@
-// Toggle Active
-
-const navbarNav = document.querySelector
-('.navbar-nav');
-// Ketika menu di klik
-document.querySelector('#menu').
-onclick = () => {
-navbarNav.classlist.toggle('active');
-};
