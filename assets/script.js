ede4e216af148493d4ba999f8460f6dd18227fc1
yvdzke <128133701+yvdzke@users.noreply.github.com>
2023-03-29 18:56:17 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// Toggle Active
+
+const navbarNav = document.querySelector
+('.navbar-nav');
+// Ketika menu di klik
+document.querySelector('#menu').
+onclick = () => {
+
+navbarNav.classList.toggle('active');
+
+};
