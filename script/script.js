5895fd55fbb6a5116d91215e87adf3879ea2da79
yvdzke <128133701+yvdzke@users.noreply.github.com>
2023-04-01 08:34:48 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,1 +1,22 @@
+// Toggle Active
+const navbarNav = document.querySelector
+('.navbar-nav');
 
+// Ketika menu di klik
+document.querySelector('#menu').
+onclick = () => {
+
+navbarNav.classList.toggle('active');
+
+};
+
+// klik di luar sidebar untuk menghilangkan navigator
+const menu = document.querySelector('#menu');
+
+document.addEventListener('click', function(e){
+
+ if(!menu.contains(e.target) && !navbarNav.contains(e.target)){
+navbarNav.classList.remove('active');
+}
+
+});
